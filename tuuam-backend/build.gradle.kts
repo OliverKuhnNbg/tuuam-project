dba58cde4bbe1c5253c91082ca2beed9c5360a6e
OliverKuhnNbg <oliver.o.kuhn@gmail.com>
2026-03-02 01:58:45 +0100

    init & setup project base

---
+++ tuuam-backend/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.11"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.tuuam"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springAiVersion"] = "1.1.2"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.ai:spring-ai-starter-model-vertex-ai-gemini")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	runtimeOnly("org.postgresql:postgresql")
+	developmentOnly("org.springframework.ai:spring-ai-spring-boot-docker-compose")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ tuuam-backend/src/test/java/com/tuuam/tuuam_backend/TuuamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuuam.tuuam_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuuamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
